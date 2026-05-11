53f41e4c825b94c32948c1a28e4f46758c7d700f
yukihayashi <yukiha617@gmail.com>
2026-05-11 11:17:21 +0900

    UIの改善: GlassBackgroundにExperimentalForeignApiのオプトインを追加して、buildエラーを修正。

--- composeApp/src/iosMain/kotlin/com/rectime/mobile/ui/component/GlassBackground.ios.kt
+++ composeApp/src/iosMain/kotlin/com/rectime/mobile/ui/component/GlassBackground.ios.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.interop.UIKitViewController
+import kotlinx.cinterop.ExperimentalForeignApi
 
+@OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun GlassBackground(modifier: Modifier) {
     val factory = GlassViewControllerRegistry.factory
