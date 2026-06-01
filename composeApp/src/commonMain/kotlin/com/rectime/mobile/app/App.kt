b0b6bc1cdfe3fd646332a1ccaaf94898212dbe5b
haru-02-code <gakkouyou0225@gmail.com>
2026-06-01 15:39:28 +0900

    commit test

--- composeApp/src/commonMain/kotlin/com/rectime/mobile/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/rectime/mobile/app/App.kt
@@ -14,6 +14,8 @@ import com.rectime.mobile.app.navigation.NavigationHost
 import com.rectime.mobile.ui.theme.AppTheme
 import com.rectime.mobile.ui.theme.ThemeStateHolder
 
+//コミットテスト
+
 @Composable
 @Preview
 fun App() {
