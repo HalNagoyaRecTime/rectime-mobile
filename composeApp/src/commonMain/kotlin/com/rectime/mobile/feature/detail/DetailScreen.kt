f296aee6de9cbba4b062c94a17d94e9f2c95ef0f
haru-02-code <gakkouyou0225@gmail.com>
2026-06-10 02:14:49 +0900

    競技の情報を確認する画面の見た目を作成

--- composeApp/src/commonMain/kotlin/com/rectime/mobile/feature/detail/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/rectime/mobile/feature/detail/DetailScreen.kt
@@ -1,9 +1,15 @@
 package com.rectime.mobile.feature.detail
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.rectime.mobile.app.navigation.NavigationController
 import com.rectime.mobile.app.navigation.Screen
@@ -15,16 +21,44 @@ data class DetailScreen(val id: String) : Screen {
 
     @Composable
     override fun Content(navigationController: NavigationController) {
+
+        //競技のダミーデータ
+        val kyougiName = "紙飛行機飛ばし"
+        val kyougiDescription =
+            "各自で作成した紙飛行機を当日持参し、決められた位置から一斉に飛ばします。"+
+            "紙飛行機が停止した地点までの距離を計測し、最も遠くまで飛ばした参加者が優勝となります。"
+
         PushScreenScaffold(
-            title = "詳細",
+            title = "競技詳細",
             onBack = { navigationController.requestPop() },
         ) {
             item {
                 Text(
-                    text = "詳細情報の表示エリアです。",
+                    text = kyougiName,
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(vertical = 12.dp),
+                )
+                Text(
+                    text = kyougiDescription,
                     color = AppTheme.colors.textSecondary,
                     modifier = Modifier.padding(vertical = 12.dp),
                 )
+            }
+            //ボタンを画面下部に配置するために説明文とボタンの間に大きめの余白を入れる
+            //画面下に固定したい場合はPushScreenScaffoldを修正する必要あり
+            item {
+                Spacer(modifier = Modifier.height(500.dp))
+            }
+            item {
+                Button(
+                    onClick = {
+                        //画面遷移先を後程追加
+                    },
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+                ){
+                    Text("呼び出し情報")
+                }
             }
         }
     }
