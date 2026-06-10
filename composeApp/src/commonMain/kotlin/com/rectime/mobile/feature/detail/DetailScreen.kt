8e314ad53b43cf7e8d4105143c94339d1157e154
haru-02-code <gakkouyou0225@gmail.com>
2026-06-11 00:07:24 +0900

    指摘事項を修正

--- composeApp/src/commonMain/kotlin/com/rectime/mobile/feature/detail/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/rectime/mobile/feature/detail/DetailScreen.kt
@@ -1,8 +1,6 @@
 package com.rectime.mobile.feature.detail
 
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
