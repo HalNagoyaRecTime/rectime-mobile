f073423e3f85903739e5dc2ed15f47ed640dce33
MayugeStudio <72333443+MayugeStudio@users.noreply.github.com>
2026-07-14 00:15:07 +0900

    feat: 通知画面の右上に更新ボタンを追加

--- composeApp/src/commonMain/kotlin/com/rectime/mobile/feature/notifications/NotificationsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/rectime/mobile/feature/notifications/NotificationsScreen.kt
@@ -2,7 +2,9 @@ package com.rectime.mobile.feature.notifications
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,6 +14,8 @@ import com.rectime.mobile.app.navigation.Screen
 import com.rectime.mobile.ui.component.PressSurface
 import com.rectime.mobile.ui.component.RootScreenScaffold
 import com.rectime.mobile.ui.theme.AppTheme
+import com.woowla.compose.icon.collections.fontawesome.fontawesome.SolidGroup
+import com.woowla.compose.icon.collections.fontawesome.fontawesome.solid.ArrowsRotate
 
 object NotificationsScreen : Screen {
     override val key: String = "notifications"
@@ -20,6 +24,15 @@ object NotificationsScreen : Screen {
     override fun Content(navigationController: NavigationController) {
         RootScreenScaffold(
             title = "通知",
+            onTrailingClick = { /* TODO: 通知一覧の再取得処理を実装予定 */ },
+            trailing = {
+                Icon(
+                    imageVector = SolidGroup.ArrowsRotate,
+                    contentDescription = "更新",
+                    tint = AppTheme.colors.textPrimary,
+                    modifier = Modifier.size(18.dp),
+                )
+            },
         ) {
             item {
                 Text(
