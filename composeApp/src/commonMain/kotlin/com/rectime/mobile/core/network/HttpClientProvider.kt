39249657df2677deb3ba1937a68bd5b822718c2c
dtnrs <dtnrs91357@yahoo.co.jp>
2026-07-20 21:23:54 +0900

    HttpClientProvider: ContentNegotiation設定を整理

--- composeApp/src/commonMain/kotlin/com/rectime/mobile/core/network/HttpClientProvider.kt
+++ composeApp/src/commonMain/kotlin/com/rectime/mobile/core/network/HttpClientProvider.kt
@@ -3,7 +3,6 @@ package com.rectime.mobile.core.network
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.http.ContentType.Application.Json
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
