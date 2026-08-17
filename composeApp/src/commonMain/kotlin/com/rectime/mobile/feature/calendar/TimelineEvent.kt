b7f4a9b14e6f4bd3dd319265cc7ebdaad60c2884
MayugeStudio <72333443+MayugeStudio@users.noreply.github.com>
2026-08-17 18:01:55 +0900

    CalendarViewModel の HTTP クライアントと現在時刻をコンストラクタで差し替えられるようにする

--- composeApp/src/commonMain/kotlin/com/rectime/mobile/feature/calendar/TimelineEvent.kt
+++ composeApp/src/commonMain/kotlin/com/rectime/mobile/feature/calendar/TimelineEvent.kt
@@ -1,5 +1,8 @@
 package com.rectime.mobile.feature.calendar
 
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.format.char
+
 data class TimelineEvent(
     val eventId: Int,
     val title: String,
@@ -11,3 +14,34 @@ data class TimelineEvent(
     val startTimeLabel: String,
     val endTimeLabel: String,
 )
+
+private val apiTimeFormat = LocalTime.Format {
+    hour()
+    minute()
+}
+
+private val displayTimeFormat = LocalTime.Format {
+    hour()
+    char(':')
+    minute()
+}
+
+internal fun EventResponse.toTimelineEvent(): TimelineEvent {
+    val start = LocalTime.parse(startTime, format = apiTimeFormat)
+    val end = LocalTime.parse(endTime, format = apiTimeFormat)
+
+    val startMinuteOfDay = start.hour * 60 + start.minute
+    val endMinuteOfDay = end.hour * 60 + end.minute
+
+    return TimelineEvent(
+        eventId = eventId,
+        title = eventName,
+        venue = venue,
+        startMinuteOfDay = startMinuteOfDay,
+        durationMinutes = endMinuteOfDay - startMinuteOfDay,
+        lane = 0,
+        laneCount = 1,
+        startTimeLabel = displayTimeFormat.format(start),
+        endTimeLabel = displayTimeFormat.format(end),
+    )
+}
