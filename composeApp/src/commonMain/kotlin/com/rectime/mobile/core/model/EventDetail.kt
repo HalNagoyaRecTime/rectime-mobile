890f8b3b8ec2f1c959628981ed00fb05b28106b1
dtnrs <dtnrs91357@yahoo.co.jp>
2026-09-22 10:54:37 +0900

    EventDetailに複数開催場所(venues)を追加

--- composeApp/src/commonMain/kotlin/com/rectime/mobile/core/model/EventDetail.kt
+++ composeApp/src/commonMain/kotlin/com/rectime/mobile/core/model/EventDetail.kt
@@ -4,6 +4,7 @@ data class EventDetail(
     val eventId: Int,
     val eventName: String,
     val venue: String,
+    val venues: List<EventVenue> = emptyList(),
     val startTime: String,
     val endTime: String,
     val ruleText: String?,
